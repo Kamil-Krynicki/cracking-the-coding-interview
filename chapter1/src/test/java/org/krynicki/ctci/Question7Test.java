d81168199a5c96f47e3f0b2fd8163ba019224ea1
Kamil-Krynicki <kamil.krynicki@gmail.com>
2016-08-30 19:54:21 +0200

    Questions  7 (initial test) chapter 1

---
+++ chapter1/src/test/java/org/krynicki/ctci/Question7Test.java
@@ -0,0 +1,19 @@
+package org.krynicki.ctci;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by K on 2016-08-29.
+ */
+public class Question7Test extends AbstractChapter1Test {
+
+    private static Question7 q = new Question7();
+
+    @Test
+    public void testRotate() throws Exception {
+        int[][] test;
+    }
+
+}
