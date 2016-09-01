fb075f8a3b727e647c580b57661d3d6adb8fa06a
Kamil-Krynicki <kamil.krynicki@gmail.com>
2016-09-01 22:24:59 +0200

    Question 9 (I knew it from before)

---
+++ chapter1/src/main/java/org/krynicki/ctci/Question9.java
@@ -0,0 +1,11 @@
+package org.krynicki.ctci;
+
+/**
+ * Created by K on 2016-09-01.
+ */
+public class Question9 {
+    // I knew this trick
+    public boolean isRotation(String s1, String s2){
+        return s1.concat(s1).contains(s2);
+    }
+}
