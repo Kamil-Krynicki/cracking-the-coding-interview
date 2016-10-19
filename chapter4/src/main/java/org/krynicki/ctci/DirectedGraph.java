8e85444f0203066c34e312594769cee08259d448
Kamil-Krynicki <kamil.krynicki@gmail.com>
2016-10-19 23:40:56 +0200

    Setup for chapter 4

---
+++ chapter4/src/main/java/org/krynicki/ctci/DirectedGraph.java
@@ -0,0 +1,47 @@
+package org.krynicki.ctci;
+
+import java.util.*;
+
+/**
+ * Created by K on 2016-10-19.
+ */
+public class DirectedGraph<T> {
+    Set<T> nodes;
+    Map<T, List<T>> edges;
+
+    public DirectedGraph() {
+        this.nodes = new HashSet<>();
+        this.edges = new HashMap<>();
+    }
+
+    public void add(T node) {
+        if(!nodes.contains(node)) {
+            nodes.add(node);
+            edges.put(node, new LinkedList<T>());
+        }
+    }
+
+    public void addEdge(T from, T to) {
+        if(!nodes.contains(from)) {
+            return;
+        }
+
+        if(!nodes.contains(to)) {
+            return;
+        }
+
+        edges.get(from).add(to);
+    }
+
+    public Iterable<T> nodes() {
+        return nodes;
+    }
+
+    public Iterable<T> edgesFrom(T node) {
+        if(!nodes.contains(node)) {
+            return null;
+        }
+
+        return edges.get(node);
+    }
+}
