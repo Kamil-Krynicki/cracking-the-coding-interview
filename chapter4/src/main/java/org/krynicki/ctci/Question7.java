35379677118971eb3a0e1fb64e3319f943021fa9
Kamil-Krynicki <kamil.krynicki@gmail.com>
2017-04-27 17:31:16 +0200

    Question 4.7 Final

--- chapter4/src/main/java/org/krynicki/ctci/Question7.java
+++ chapter4/src/main/java/org/krynicki/ctci/Question7.java
@@ -1,45 +1,65 @@
 package org.krynicki.ctci;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 import java.util.Deque;
+import java.util.List;
 import java.util.Set;
 
 /**
  * Created by K on 24.04.2017.
  */
 public class Question7 {
-    // basic topological sort
+    public static class TopologicalSort<T> {
+        private Set<T> done = Sets.newHashSet();
+        private Set<T> visits = Sets.newHashSet();
 
-    Set<Integer> done = Sets.newHashSet();
-    Deque<Integer> order = Lists.newLinkedList();
+        private Deque<T> order = Lists.newLinkedList();
 
-    public void solve(DirectedGraph<Integer> graph) {
-        for(Integer node : graph.nodes()) {
-            if(!done.contains(node)) {
-                Deque<Integer> visiting = Lists.newLinkedList();
-                inDepth(node, graph, visiting);
+        public TopologicalSort(DirectedGraph<T> graph) {
+            solve(graph);
+        }
+
+        public List<T> getOrder() {
+            return ImmutableList.copyOf(order);
+        }
+
+        private void solve(DirectedGraph<T> graph) {
+            Preconditions.checkNotNull(graph);
+
+            for(T node : graph.nodes()) {
+                if(!explored(node)) {
+                    explore(node, graph);
+                }
             }
         }
 
-        System.out.println(order);
-    }
+        private void explore(T node, DirectedGraph<T> graph) {
+            for(T neighbour : graph.neighbours(node)) {
+                if(revisiting(neighbour)) {
+                    throw new IllegalArgumentException("cycle");
+                }
 
-    public void inDepth(Integer node, DirectedGraph<Integer> graph, Deque<Integer> visiting) {
-        for(Integer neighbour : graph.neighbours(node)) {
-            if(visiting.contains(neighbour)) {
-                throw new IllegalArgumentException("cycle");
+                if (!explored(neighbour)) {
+                    visits.add(neighbour);
+                    explore(neighbour, graph);
+                    visits.remove(neighbour);
+                }
             }
 
-            if (!done.contains(neighbour)) {
-                visiting.add(neighbour);
-                inDepth(neighbour, graph, visiting);
-                visiting.remove(neighbour);
-            }
+            done.add(node);
+            order.addLast(node);
         }
 
-        done.add(node);
-        order.push(node);
+        private boolean revisiting(T neighbour) {
+            return visits.contains(neighbour);
+        }
+
+        private boolean explored(T neighbour) {
+            return done.contains(neighbour);
+        }
     }
 }
--- chapter4/src/test/java/org/krynicki/ctci/Question7Test.java
+++ chapter4/src/test/java/org/krynicki/ctci/Question7Test.java
@@ -1,24 +1,48 @@
 package org.krynicki.ctci;
 
+import com.google.common.collect.Lists;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.krynicki.ctci.Question7.TopologicalSort;
 
 public class Question7Test {
 
+    @Test(expectedExceptions = NullPointerException.class)
+    public void shouldHandleNull() throws Exception {
+        new TopologicalSort<>(null).getOrder();
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void shouldDetectCycle() throws Exception {
+
+        DirectedGraph<Integer> graph = new DirectedGraph<>();
+
+        graph.addNode(4);
+        graph.addNode(3);
+        graph.addNode(2);
+        graph.addNode(1);
+
+        graph.addEdge(2, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(3, 4);
+
+        new TopologicalSort<>(graph).getOrder();
+    }
+
     @Test
-    public void testSolve() throws Exception {
-        Question7 test = new Question7();
+    public void shouldSolveCycle() throws Exception {
+        DirectedGraph<Integer> graph = new DirectedGraph<>();
 
-        DirectedGraph<Integer> kamil = new DirectedGraph<>();
+        graph.addNode(4);
+        graph.addNode(3);
+        graph.addNode(2);
+        graph.addNode(1);
 
-        kamil.addNode(4);
-        kamil.addNode(3);
-        kamil.addNode(2);
-        kamil.addNode(1);
+        graph.addEdge(2, 1);
+        graph.addEdge(1, 3);
+        graph.addEdge(3, 4);
 
-        kamil.addEdge(2, 1);
-        kamil.addEdge(1, 3);
-        kamil.addEdge(3, 4);
-
-        test.solve(kamil);
+        Assert.assertEquals(new TopologicalSort<>(graph).getOrder(), Lists.newArrayList(4, 3, 1, 2));
     }
 }
