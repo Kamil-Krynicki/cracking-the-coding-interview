14c274dc7404ace26f1e4a32b08b4cbb954518b5
Kamil-Krynicki <kamil.krynicki@gmail.com>
2017-04-28 13:14:49 +0200

    Question 4.10 number change

---
+++ chapter4/src/main/java/org/krynicki/ctci/Question10.java
@@ -0,0 +1,33 @@
+package org.krynicki.ctci;
+
+import com.google.common.collect.Lists;
+
+import java.util.Deque;
+
+/**
+ * Created by kamil.krynicki on 27/04/2017.
+ */
+public class Question10 {
+    void findPaths(TreeNode<Integer> root, int value) {
+        innerFind(Lists.<TreeNode<Integer>>newLinkedList(), value, root);
+    }
+
+    void innerFind(Deque<TreeNode<Integer>> currentPath, int valueLeft,
+                   TreeNode<Integer> current) {
+        if (current == null)
+            return;
+
+        currentPath.addLast(current);
+        int newValue = valueLeft - current.data;
+
+        if (newValue < 0) {
+            newValue += currentPath.getFirst().data;
+            currentPath.removeFirst();
+        } else if (newValue == 0) {
+            System.out.println(currentPath);
+        }
+
+        innerFind(Lists.newLinkedList(currentPath), newValue, current.left());
+        innerFind(Lists.newLinkedList(currentPath), newValue, current.right());
+    }
+}
