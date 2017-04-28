5b1149e13d4f7046a9a9139bf1b6180bb3decc7a
Kamil-Krynicki <kamil.krynicki@gmail.com>
2017-04-28 13:27:34 +0200

    Question 4.8 improvements + tests

--- chapter4/src/main/java/org/krynicki/ctci/Question8.java
+++ chapter4/src/main/java/org/krynicki/ctci/Question8.java
@@ -1,33 +1,72 @@
 package org.krynicki.ctci;
 
-import com.google.common.collect.Lists;
+import com.google.common.base.MoreObjects;
 
-import java.util.Deque;
+import java.util.LinkedList;
 
 /**
- * Created by kamil.krynicki on 27/04/2017.
+ * Created by kamil.krynicki on 28/04/2017.
  */
 public class Question8 {
-    void findPaths(TreeNode<Integer> root, int value) {
-        innerFind(Lists.<TreeNode<Integer>>newLinkedList(), value, root);
-    }
+    public static class FirstCommonAncestor {
+        public <T> TreeNode<T> fca(TreeNode<T> nodeA, TreeNode<T> nodeB) {
+            if(nodeA == null || nodeB == null) return null;
 
-    void innerFind(Deque<TreeNode<Integer>> currentPath, int valueLeft,
-                   TreeNode<Integer> current) {
-        if (current == null)
-            return;
+            if(nodeA == nodeB) return nodeA;
 
-        currentPath.addLast(current);
-        int newValue = valueLeft - current.data;
+            LinkedList<TreeNode<T>> rootToNodeA = parentSequence(nodeA);
+            LinkedList<TreeNode<T>> rootToNodeB = parentSequence(nodeB);
 
-        if (newValue < 0) {
-            newValue += currentPath.getFirst().data;
-            currentPath.removeFirst();
-        } else if (newValue == 0) {
-            System.out.println(currentPath);
+            TreeNode<T> prv = null;
+            TreeNode<T> headA, headB;
+
+            while(!rootToNodeA.isEmpty() && !rootToNodeB.isEmpty()) {
+                headA = rootToNodeA.pollFirst();
+                headB = rootToNodeB.pollFirst();
+
+                if (headA == headB)
+                    prv = headA;
+                else
+                    return prv;
+            }
+
+            return prv;
         }
 
-        innerFind(Lists.newLinkedList(currentPath), newValue, current.left());
-        innerFind(Lists.newLinkedList(currentPath), newValue, current.right());
+        // with parent pointers O(logn)
+        private <T> LinkedList<TreeNode<T>> parentSequence(TreeNode<T> node) {
+            LinkedList<TreeNode<T>> result = new LinkedList<>();
+
+            TreeNode<T> current = node;
+
+            while(current != null) {
+                result.addFirst(current);
+                current = current.parent();
+            }
+
+            return result;
+        }
+
+        //without parentPointers O(n)
+        private <T> LinkedList<TreeNode<T>> parentSequence(TreeNode<T> node, TreeNode<T> root) {
+            if(root == null) return null;
+
+            LinkedList<TreeNode<T>> result = singleton(node);
+
+            if(root == node) return result;
+
+            LinkedList<TreeNode<T>> l = parentSequence(node, root.left());
+            LinkedList<TreeNode<T>> r = parentSequence(node, root.right());
+
+            result.addAll(MoreObjects.firstNonNull(l, r));
+
+            return result;
+        }
+
+        private <T> LinkedList<TreeNode<T>> singleton(TreeNode<T> node) {
+            LinkedList<TreeNode<T>> result = new LinkedList<>();
+            result.add(node);
+            return result;
+        }
     }
 }
--- chapter4/src/test/java/org/krynicki/ctci/Question8Test.java
+++ chapter4/src/test/java/org/krynicki/ctci/Question8Test.java
@@ -1,17 +1,66 @@
 package org.krynicki.ctci;
 
+import junit.framework.Assert;
 import org.testng.annotations.Test;
 
+import static org.krynicki.ctci.Question8.FirstCommonAncestor;
 import static org.krynicki.ctci.TreeNode.node;
+import static org.mockito.Mockito.mock;
 
 public class Question8Test {
     @Test
-    public void shouldFindSuccessorOfLast() {
-        TreeNode<Integer> root =
-                node(3).withLeft(node(8)).withRight(node(2));
-
-        Question8 alg = new Question8();
-
-        alg.findPaths(root, 5);
+    public void shouldHandleNull() throws Exception {
+        Assert.assertEquals(new FirstCommonAncestor().fca(null, null), null);
+        Assert.assertEquals(new FirstCommonAncestor().fca(mock(TreeNode.class), null), null);
+        Assert.assertEquals(new FirstCommonAncestor().fca(null, mock(TreeNode.class)), null);
     }
+
+    @Test
+    public void shouldFindSimpleParent() throws Exception {
+        TreeNode<Integer> nodeA, nodeB, common;
+
+        TreeNode<Integer> root = common = node(5)
+                .withLeft(nodeB = node(3))
+                .withRight(nodeA = node(9).withLeft(node(7)));
+
+        Assert.assertEquals(new FirstCommonAncestor().fca(nodeA, nodeB), common);
+    }
+
+    @Test
+    public void shouldFindSameNode() throws Exception {
+        TreeNode<Integer> nodeA, nodeB, common;
+
+        TreeNode<Integer> root = node(5)
+                .withLeft(node(3))
+                .withRight(common = nodeA = nodeB = node(9).withLeft(node(7)));
+
+        Assert.assertEquals(new FirstCommonAncestor().fca(nodeA, nodeB), common);
+    }
+
+    @Test
+    public void shouldFindSimpleParentInOneOfNodes() throws Exception {
+        TreeNode<Integer> nodeA, nodeB, common;
+
+        TreeNode<Integer> root = node(5)
+                .withLeft(node(3))
+                .withRight(common = nodeA = node(9).withLeft(nodeB = node(7)));
+
+        Assert.assertEquals(new FirstCommonAncestor().fca(nodeA, nodeB), common);
+    }
+
+    @Test
+    public void shouldReturnNullOnDifferedTrees() throws Exception {
+        TreeNode<Integer> nodeA, nodeB, common = null;
+
+        TreeNode<Integer> rootA = node(5)
+                .withLeft(node(3))
+                .withRight(node(9).withLeft(nodeA = node(7)));
+
+        TreeNode<Integer> rootB = node(5)
+                .withLeft(node(3))
+                .withRight(nodeB = node(9).withLeft(node(7)));
+
+        Assert.assertEquals(new FirstCommonAncestor().fca(nodeA, nodeB), common);
+    }
+
 }
