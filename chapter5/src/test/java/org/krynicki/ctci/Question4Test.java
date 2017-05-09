d66ad56c44abb5a1f1b145a981a0905cdf2091f2
Kamil-Krynicki <kamil.krynicki@gmail.com>
2017-05-09 18:05:04 +0200

    Question 5.4 silly error

--- chapter5/src/test/java/org/krynicki/ctci/Question4Test.java
+++ chapter5/src/test/java/org/krynicki/ctci/Question4Test.java
@@ -8,8 +8,5 @@ public class Question4Test {
     @Test
     public void test() {
 
-        Assert.assertEquals(new Question4().nextSmallestSameOnes(0b11011101111), 8);
-        Assert.assertEquals(new Question4().nextSmallestSameOnes(0b1100111001111), 5);
-        Assert.assertEquals(new Question4().nextSmallestSameOnes(0b110001110001111), 5);
     }
 }
