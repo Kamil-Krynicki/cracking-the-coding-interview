104653438cb7817e9a1f972c7c010a1ac04e6826
Kamil-Krynicki <kamil.krynicki@gmail.com>
2017-05-18 15:03:32 +0200

    Question 56 test made public

--- chapter5/src/test/java/org/krynicki/ctci/Question6Test.java
+++ chapter5/src/test/java/org/krynicki/ctci/Question6Test.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 public class Question6Test {
     @Test
-    private void shouldWorkFine() {
+    public void shouldWorkFine() {
         Assert.assertEquals(new Question6().flipDistance(29, 15), 2);
     }
 }
