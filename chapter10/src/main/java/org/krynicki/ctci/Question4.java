980134a1521feac7aa8f4a6bd6c0299511e4f990
Kamil-Krynicki <kamil.krynicki@gmail.com>
2017-07-06 18:07:11 +0200

    Question 10.4 initial idea

---
+++ chapter10/src/main/java/org/krynicki/ctci/Question4.java
@@ -0,0 +1,56 @@
+package org.krynicki.ctci;
+
+/**
+ * Created by kamil.krynicki on 06/07/2017.
+ */
+public class Question4 {
+
+    public int find(Listy data, int key) {
+        return find(data, key, 0, 1);
+    }
+
+    private int find(Listy data, int key, int from, int to) {
+        if(from == to)
+            return -1;
+
+        int mid = (to + from) / 2;
+
+        if(data.elementAt(mid) == key)
+            return mid;
+
+        if(data.elementAt(mid) == -1 || data.elementAt(mid) > key)
+            return find(data, key, from, mid);
+
+        if(data.elementAt(mid) < key)
+            if(data.elementAt(to) > key)
+                return find(data, key, mid, to);
+            else
+                return find(data, key, mid, 2 * to);
+
+        return -1;
+    }
+
+    class Listy {
+        private final int[] data;
+        private final int size;
+
+        public Listy(int... data) {
+            this.size = data.length;
+            this.data = new int[size];
+
+            System.arraycopy(data, 0, this.data, 0, this.size);
+        }
+
+        public int elementAt(int i) {
+            if (insideBounds(i))
+                return data[i];
+            else
+                return -1;
+        }
+
+        private boolean insideBounds(int i) {
+            return i < this.size;
+        }
+    }
+
+}
